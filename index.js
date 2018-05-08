1d6e34f52ca6def36d1ce4d036ea3ad7e6519f46
Wotan <11673286+Rick-Houser@users.noreply.github.com>
2018-05-08 11:20:42 -0700

    update index.js with createServer()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
