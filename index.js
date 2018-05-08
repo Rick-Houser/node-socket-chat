2a5ced418465a82f83473dbcbf23d26bb3de20bb
Wotan <11673286+Rick-Houser@users.noreply.github.com>
2018-05-08 12:53:57 -0700

    updated to latest socket.io.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var app = require('express')();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 server.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('send msg from server');
+    console.log(msg)
+    io.emit('chat message', msg);
+  });
+});
+
